8fc94b874ad48c5c42c2bb096c436e186c400db3
Shane Witbeck <shane@surly.dev>
2024-10-25 08:56:42 -0700

    feat: Mount shared volume

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,8 @@
 
 set -e
 
+VOLUME_1="0HC_Volume_101503771"
+
 function install_podman() {
     curl -fsSL -o podman-linux-amd64.tar.gz https://github.com/mgoltzsche/podman-static/releases/latest/download/podman-linux-amd64.tar.gz
     tar -xzf podman-linux-amd64.tar.gz
@@ -20,8 +22,15 @@ function enable_podman_socket() {
     sudo systemctl enable --now podman.socket
 }
 
+function mount_volume_1() {
+  mkdir /mnt/volume-1
+  mount -o discard,defaults /dev/disk/by-id/scsi-${VOLUME_1} /mnt/volume-1
+  echo "/dev/disk/by-id/scsi-${VOLUME_1} /mnt/volume-1 ext4 discard,nofail,defaults 0 0" >> /etc/fstab
+}
+
 install_podman
 enable_podman_service
 enable_podman_socket
+mount_volume_1
 
 echo "Done!"
